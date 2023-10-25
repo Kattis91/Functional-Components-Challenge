8a821d44b1bccf21207faf9f8d7409d10db0db8c
Kattis91 <katya.svedmark@gmail.com>
2023-10-25 21:54:57 +0000

    NavBarImple challenge

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -3,7 +3,7 @@ import css from "./css/Sidebar.module.css"
 
 function Sidebar () {
     return (
-        <div div className={css.sidebar} >
+        <div className={css.sidebar} >
             <a href="#">My Photos</a>
             <a href="#">My Illustrations</a>
             <a href="#">MyPaintings</a>
